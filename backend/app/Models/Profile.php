fd4c12b60c52d4aa7f35285767512fe5a3717e1e
Matheus Barbosa <matheus.barbosah@gmail.com>
2025-06-09 08:12:03 -0300

    feat(backend/Models): Profile add fillables

--- backend/app/Models/Profile.php
+++ backend/app/Models/Profile.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Profile extends Model
 {
     /** @use HasFactory<\Database\Factories\ProfileFactory> */
     use HasFactory;
 
-    public function users()
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
     }
